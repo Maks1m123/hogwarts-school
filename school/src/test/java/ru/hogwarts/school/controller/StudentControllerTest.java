594cd324c6c7afab5911863302ba032ec76e2457
Максим <lonelymaks123@gmail.com>
2026-09-16 12:07:52 +0300

    hwDataBase homeWork4

---
+++ school/src/test/java/ru/hogwarts/school/controller/StudentControllerTest.java
@@ -0,0 +1,27 @@
+package ru.hogwarts.school.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.test.web.server.LocalServerPort;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class StudentControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    @Autowired
+    private StudentController studentController;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Test
+    void contextLoads() {
+        assertThat(studentController).isNotNull();
+    }
+}
